8a532f1f130e5465b34df17f02e9344373ddf548
404SAMfound <ssam.erp6430@gmail.com>
2025-09-23 08:39:11 +0000

    datatype commit

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) we are using node js not web browser
+
+console.log(3+3); 
+console.log("samridhi")
+
+let name
+let age
+let isloggedin
+
+//number => 2 pwr 53
+// bigint
+// string => ""
+// boolean => true/false, yes/no
+// null => standalone value 
+// undefined => value not assigned
+// symbol => uniqness
+// object
+
+console.log(typeof "6")  // to get to knoe the type of value
---
+++ 01_basics/variable.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "samarsh@google.com"
+var accountPassword = "12345"
+accountCity = "dhanbad" // better not use js know ki ts is also a method
+let accountState 
+// accountId = 2 // not allowed cuz const
+
+
+
+accountEmail = "ssamerp@g.com"
+accountPassword = "1234567"
+accountCity = "pune"
+console.log(accountId);
+
+/*
+pls dont use var cuz of issue in block scope nd fucntional scope
+*/
+
+console.table([accountId, accountPassword, accountEmail, accountCity, accountState])
