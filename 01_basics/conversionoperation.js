5dced256099d063b5fff98c8c70270396e351d8a
404SAMfound <ssam.erp6430@gmail.com>
2025-09-23 09:12:30 +0000

    operation conversion done

---
+++ 01_basics/conversionoperation.js
@@ -0,0 +1,22 @@
+let score = "33abd" // undefined likhenge toh bhi NaN ayega
+let scores = 33
+let skore = null
+console.log(typeof score);
+console.log(typeof scores);
+console.log(typeof skore);
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let valueINumber = String(skore)
+console.log(typeof valueINumber);
+console.log(valueINumber);
+ 
+let valueinNumber = Number(skore)
+console.log(typeof valueinNumber);
+console.log(valueinNumber);
+
+// true => 1; false => 0, boolean me convert ho jayega, valueinnumber nikalenge uska toh
+// "" => false ; "anything" => true  (ye ho gya valueinboolean)
