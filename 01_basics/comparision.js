82e216fdfac71393e1fe74623e1ad0ca19e64f5c
404SAMfound <ssam.erp6430@gmail.com>
2025-10-03 04:39:57 +0000

    config 3 done

---
+++ 01_basics/comparision.js
@@ -0,0 +1,17 @@
+/*console.log(2>1)
+console.log(2>=1)
+console.log(2==1)
+console.log(2<1)
+console.log(2!=1) */
+
+console.log("2" > 1)   // js automatically 2 ko number me covert kr diya
+console.log("02" > 1)  //altho is adviced ki alag alag datatype ko compare na kre
+
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+
+console.log("2" ==2)  // double equality me wo true dikha dega kyuki conversions hone lgte but tripple equality me
+                      //  strictly checking hoti like isme datatype bhi check hoga nd false answer de dega
+
+
---
+++ 01_basics/dataypesummary.js
@@ -0,0 +1,23 @@
+// primitive 
+
+// 7 types = string : number : boolean : null : undefined : symbol : bigint
+
+// reference type of non primitive
+
+// types = array, objecs, functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+// object
+
+  let MyObj
+{
+  name : "samridhi",
+    age ; 18,
+}
+
+// function
+
+const MyFun = function(){}
+    console.log("hello ji");
+    
+    
