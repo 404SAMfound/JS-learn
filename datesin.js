5bbafb6e4c8bbce89681cf399aa71c0818a9c397
404SAMfound <ssam.erp6430@gmail.com>
2025-12-12 15:32:49 +0000

    done datesinjs

---
+++ datesin.js
@@ -0,0 +1,10 @@
+let todaysdate = new date()
+console.log(todaysdate.toString());
+console.log(todaysdate.toDateString());
+console.log(todaysdate.toLocaleString());
+console.log(typeof todaysdate);
+
+let createddate = new date(2025, 11, 12)
+let createddate = new date(2025, 11, 12, 20, 52)
+let createddate = new date("2025-12-12")
+console.log(createddate.toDateStrings());
